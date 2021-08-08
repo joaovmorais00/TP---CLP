42b17073d0573db1ea11defeab5e64812170706f
igororamos <igororamos@gmail.com>
2021-08-08 18:27:00 -0300

    pessoa

---
+++ tp_CLP_PHP/Pessoa.php
@@ -0,0 +1,44 @@
+<?php 
+abstract class Pessoa{
+	private $nome;
+	private $endereco;
+
+class Cliente extends Pessoa{
+	private $RG;
+	private $data_de_nascimento;
+	private $quantidade;
+	
+	
+	public function get_nome(){
+		return $this->nome;
+	}
+	public function get_endereco(){
+		return $this->endereco;
+	}
+	public function get_RG(){
+		return $this->RG;
+	}
+	public function get_data_de_nascimento(){
+		return $this->data_de_nascimento;
+	}
+	public function set_nome($nome){
+		$this->nome = $nome;
+	}
+	public function set_endereco($endereco){
+		$this->endereco = $endereco ;
+	}
+	public function set_RG($RG){
+		$this->RG = $RG;
+	}
+	public function set_data_de_nascimento($data_de_nascimento){
+		$this->data_de_nascimento = $data_de_nascimento;
+	}
+	public function addiciona_quantidade(){
+		$this->quantidade++;
+	}
+	public function diminue_quantidade(){
+		$this->quantidade--;
+	}
+	
+}
+?>
